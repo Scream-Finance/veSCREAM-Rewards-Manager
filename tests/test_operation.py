dde89650545cf1eab0400318b28b10acf1bcccb6
dudesahn <dudesahn@pm.me>
2023-03-23 18:11:38 -0400

    feat: ready to deploy

--- tests/test_operation.py
+++ tests/test_operation.py
@@ -1,135 +1,75 @@
 import brownie
-from brownie import Contract
-import pytest
+from brownie import Contract, chain
 
+# test all of our functions
+def test_operation(gov, scream, management, user, rewards_manager):
+    # transfer gov to v2 MS
+    rewards_manager.transferOwnership(gov, {"from": management})
 
-def test_operation(
-    chain, accounts, token, vault, strategy, user, strategist, amount, RELATIVE_APPROX
-):
-    # Deposit to the vault
-    user_balance_before = token.balanceOf(user)
-    token.approve(vault.address, amount, {"from": user})
-    vault.deposit(amount, {"from": user})
-    assert token.balanceOf(vault.address) == amount
+    # only owner can call
+    with brownie.reverts():
+        rewards_manager.setAuthorized(gov, True, {"from": user})
 
-    # harvest
-    chain.sleep(1)
-    strategy.harvest()
-    assert pytest.approx(strategy.estimatedTotalAssets(), rel=RELATIVE_APPROX) == amount
+    # give gov manager powers
+    rewards_manager.setAuthorized(gov, True, {"from": gov})
 
-    # tend()
-    strategy.tend()
+    # transfer tokens to our contract
+    scream.transfer(rewards_manager, 11_760e18, {"from": gov})
 
-    # withdrawal
-    vault.withdraw({"from": user})
-    assert (
-        pytest.approx(token.balanceOf(user), rel=RELATIVE_APPROX) == user_balance_before
-    )
+    # sleep 6 days (writing this test on a Thursday)
+    chain.sleep(86400 * 6)
 
+    # only managers can call
+    with brownie.reverts():
+        rewards_manager.weeklyPayoutAndCheckpoint({"from": user})
 
-def test_emergency_exit(
-    chain, accounts, token, vault, strategy, user, strategist, amount, RELATIVE_APPROX
-):
-    # Deposit to the vault
-    token.approve(vault.address, amount, {"from": user})
-    vault.deposit(amount, {"from": user})
-    chain.sleep(1)
-    strategy.harvest()
-    assert pytest.approx(strategy.estimatedTotalAssets(), rel=RELATIVE_APPROX) == amount
+    # confirm that we update our payout time
+    before_time = rewards_manager.lastPayoutTime()
+    before_amount = rewards_manager.lastPayoutAmount()
+    rewards_manager.weeklyPayoutAndCheckpoint({"from": management})
+    assert rewards_manager.lastPayoutTime() > before_time
+    assert rewards_manager.lastPayoutAmount() < before_amount
 
-    # set emergency and exit
-    strategy.setEmergencyExit()
-    chain.sleep(1)
-    strategy.harvest()
-    assert strategy.estimatedTotalAssets() < amount
+    # we shouldn't be able to call it again until the next epoch
+    with brownie.reverts():
+        rewards_manager.weeklyPayoutAndCheckpoint({"from": management})
 
+    # sleep for a day
+    chain.sleep(86400)
 
-def test_profitable_harvest(
-    chain, accounts, token, vault, strategy, user, strategist, amount, RELATIVE_APPROX
-):
-    # Deposit to the vault
-    token.approve(vault.address, amount, {"from": user})
-    vault.deposit(amount, {"from": user})
-    assert token.balanceOf(vault.address) == amount
+    # checkpoint our new week
+    rewards_manager.weeklyPayoutAndCheckpoint({"from": management})
 
-    # Harvest 1: Send funds through the strategy
-    chain.sleep(1)
-    strategy.harvest()
-    assert pytest.approx(strategy.estimatedTotalAssets(), rel=RELATIVE_APPROX) == amount
+    # get to the end
+    for i in range(47):
+        # sleep, queue our profits
+        chain.sleep(5 * 86400)
+        rewards_manager.weeklyPayoutAndCheckpoint({"from": management})
+        print("\nTook profits for week", int(i + 1))
+        print("Payout of:", int(rewards_manager.lastPayoutAmount() / 1e18), "SCREAM")
 
-    # TODO: Add some code before harvest #2 to simulate earning yield
+        # sleep, checkpoint our new week
+        chain.sleep(2 * 86400)
+        rewards_manager.weeklyPayoutAndCheckpoint({"from": management})
+        print("Checkpoint for week", int(i + 2))
 
-    # Harvest 2: Realize profit
-    chain.sleep(1)
-    strategy.harvest()
-    chain.sleep(3600 * 6)  # 6 hrs needed for profits to unlock
-    chain.mine(1)
-    profit = token.balanceOf(vault.address)  # Profits go to vault
-    # TODO: Uncomment the lines below
-    # assert token.balanceOf(strategy) + profit > amount
-    # assert vault.pricePerShare() > before_pps
+    # make sure we are at the end
+    lastPayout = rewards_manager.lastPayoutAmount()
+    assert lastPayout == 10e18
 
+    # we shouldn't be able to call it again, and contract should be empty
+    with brownie.reverts():
+        rewards_manager.weeklyPayoutAndCheckpoint({"from": management})
+    assert scream.balanceOf(rewards_manager) == 0
 
-def test_change_debt(
-    chain, gov, token, vault, strategy, user, strategist, amount, RELATIVE_APPROX
-):
-    # Deposit to the vault and harvest
-    token.approve(vault.address, amount, {"from": user})
-    vault.deposit(amount, {"from": user})
-    vault.updateStrategyDebtRatio(strategy.address, 5_000, {"from": gov})
-    chain.sleep(1)
-    strategy.harvest()
-    half = int(amount / 2)
+    # send in some more scream to test our sweep
+    scream.transfer(rewards_manager, 11_760e18, {"from": gov})
+    assert scream.balanceOf(rewards_manager) > 0
 
-    assert pytest.approx(strategy.estimatedTotalAssets(), rel=RELATIVE_APPROX) == half
+    # only gov can sweep
+    with brownie.reverts():
+        rewards_manager.sweep(scream, 11_760e18, {"from": management})
 
-    vault.updateStrategyDebtRatio(strategy.address, 10_000, {"from": gov})
-    chain.sleep(1)
-    strategy.harvest()
-    assert pytest.approx(strategy.estimatedTotalAssets(), rel=RELATIVE_APPROX) == amount
-
-    # In order to pass this tests, you will need to implement prepareReturn.
-    # TODO: uncomment the following lines.
-    # vault.updateStrategyDebtRatio(strategy.address, 5_000, {"from": gov})
-    # chain.sleep(1)
-    # strategy.harvest()
-    # assert pytest.approx(strategy.estimatedTotalAssets(), rel=RELATIVE_APPROX) == half
-
-
-def test_sweep(gov, vault, strategy, token, user, amount, weth, weth_amount):
-    # Strategy want token doesn't work
-    token.transfer(strategy, amount, {"from": user})
-    assert token.address == strategy.want()
-    assert token.balanceOf(strategy) > 0
-    with brownie.reverts("!want"):
-        strategy.sweep(token, {"from": gov})
-
-    # Vault share token doesn't work
-    with brownie.reverts("!shares"):
-        strategy.sweep(vault.address, {"from": gov})
-
-    # TODO: If you add protected tokens to the strategy.
-    # Protected token doesn't work
-    # with brownie.reverts("!protected"):
-    #     strategy.sweep(strategy.protectedToken(), {"from": gov})
-
-    before_balance = weth.balanceOf(gov)
-    weth.transfer(strategy, weth_amount, {"from": user})
-    assert weth.address != strategy.want()
-    assert weth.balanceOf(user) == 0
-    strategy.sweep(weth, {"from": gov})
-    assert weth.balanceOf(gov) == weth_amount + before_balance
-
-
-def test_triggers(
-    chain, gov, vault, strategy, token, amount, user, weth, weth_amount, strategist
-):
-    # Deposit to the vault and harvest
-    token.approve(vault.address, amount, {"from": user})
-    vault.deposit(amount, {"from": user})
-    vault.updateStrategyDebtRatio(strategy.address, 5_000, {"from": gov})
-    chain.sleep(1)
-    strategy.harvest()
-
-    strategy.harvestTrigger(0)
-    strategy.tendTrigger(0)
+    # sweep it out
+    rewards_manager.sweep(scream, 11_760e18, {"from": gov})
+    assert scream.balanceOf(rewards_manager) == 0
